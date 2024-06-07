933fec49dfd7a6bf4ae6e220b0e2fc504ee52674
Juanse Cipolat <jscipolat@gmail.com>
2024-06-07 19:43:06 -0300

    react1

---
+++ front/src/components/layout/Nav.js
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+const Nav = (props) => {
+    return (
+        <nav>
+            <div>
+                <ul>
+                    <li><NavLink to="/" activeClassName="activo">Home</NavLink></li>
+                    <li><NavLink to="/noticias" activeClassName="activo">Noticias</NavLink></li>
+                    <li><NavLink to="/retro" activeClassName="activo">Retro</NavLink></li>
+                    <li><NavLink to="/reviews" activeClassName="activo">Reviews</NavLink></li>
+                    <li><NavLink to="/shop" activeClassName="activo">Shop</NavLink></li>
+                    <li><NavLink to="/contacto" activeClassName="activo">Contacto</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
---
+++ front/src/pages/Contacto.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Contacto = () => {
+  return (
+    <main>
+      <h2>Contacto</h2>
+      <section className="seccion-contacto">
+        <form action="#" method="post">
+          <div className="grupo-formulario">
+            <label htmlFor="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" required />
+          </div>
+          <div className="grupo-formulario">
+            <label htmlFor="email">Correo electrónico:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="grupo-formulario">
+            <label htmlFor="mensaje">Mensaje:</label>
+            <textarea id="mensaje" name="mensaje" rows="5" required></textarea>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default Contacto;
